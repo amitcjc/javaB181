0339478864969f3d2855526b5551b74c4cfdbfcf
amitcjc <156275076+amitcjc@users.noreply.github.com>
2024-11-13 10:50:09 +0530

    m4 Method

--- DemoB181/src/com/Demo.java
+++ DemoB181/src/com/Demo.java
@@ -16,6 +16,11 @@ public class Demo
 	{
 		System.out.println("This is M3...");
 	}
+
+	public void m4()
+	{
+		System.out.println("This is M4...");
+	}
 	
 
 	public static void main(String[] args) 
